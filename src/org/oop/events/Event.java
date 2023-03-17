319d87e0c61d11c9eb89390326aa44eec42c86e0
LuisJann <giannoneluigi10@gmail.com>
2023-03-17 11:36:36 +0100

    First milestone + test

--- src/org/oop/events/Event.java
+++ src/org/oop/events/Event.java
@@ -1,10 +1,11 @@
 package org.oop.events;
 
 import java.text.DateFormat;
+import java.time.LocalDate;
 
 public class Event {
     private String title;
-    private DateFormat date;
+    private LocalDate date = LocalDate.now();
     private int numSeats;
     private int reservedSeats = 0;
 
@@ -12,7 +13,7 @@ public class Event {
         return title;
     }
 
-    public DateFormat getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
@@ -28,15 +29,38 @@ public class Event {
         this.title = title;
     }
 
-    public void setDate(DateFormat date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
-    public Event(String title, DateFormat date, int numSeats, int reservedSeats) {
+    public Event(String title, LocalDate date, int numSeats, int reservedSeats) {
         this.title = title;
         this.date = date;
         this.numSeats = numSeats;
         this.reservedSeats = reservedSeats;
+    }
+
+    public void booking(){
+        if (reservedSeats > (numSeats - reservedSeats)){
+            throw new RuntimeException("Mi dispiace la tua prenotazione non puoi essere inserita," +
+                    " perchè non ci sono abbastanza posti disponibili");
+        }
+        if (date.isBefore(LocalDate.now())){
+            throw new RuntimeException("La data inserita non è valida");
+        }
+
+        reservedSeats++;
+    }
+
+    public void canceling(){
+        if (reservedSeats == 0){
+            throw new RuntimeException("Non ci sono prenotazioni");
+        }
+        if (date.isBefore(LocalDate.now())){
+            throw new RuntimeException("L'evento è già passato");
+        }
+
+        reservedSeats--;
     }
 
     @Override
--- src/org/oop/events/Main.java
+++ src/org/oop/events/Main.java
@@ -1,7 +1,45 @@
 package org.oop.events;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        Event event = new Event("", LocalDate.now(),2,0);
+
+        boolean check = false;
+
+        System.out.println("ecco il numero di posti disponibili: " + event.getNumSeats());
+        System.out.println("Inserisci il nome dell'evento");
+        event.setTitle(scan.next());
+
+
+
+        while (!check){
+
+            System.out.println("Se vuoi prenotare un posto premi 1");
+            System.out.println("Se vuoi cancellare la prenotazione di un posto premi 2");
+            System.out.println("Per uscire premi 3");
+            int userChoice = scan.nextInt();
+
+            switch (userChoice){
+                case 1 ->{
+                    event.booking();
+                    System.out.println("il tuo posto è stato prenotato");
+                    System.out.println("Rimangono " + (event.getNumSeats() - event.getReservedSeats()) + " posti");
+                }
+                case 2 ->{
+                    event.canceling();
+                    System.out.println("Hai cancellato la prenotazione");
+                    System.out.println("I posti disponibili sono: " + (event.getNumSeats() - event.getReservedSeats() + " posti"));
+                }
+                case 3->{
+                    System.out.println("exit");
+                    check = true;
+                }
+            }
+        }
+            System.out.println(event);
     }
 }
